91cb10c9f23892f508fa1b0c2b510e741fea0ab5
bayraaqi <bayraabazaraa@gmail.com>
2020-05-25 23:00:17 +0800

    app.js oorchlogdson

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-var uiController = (function () {
-  console.log("helloo");
-})();
+// Дэлгэцтэй ажиллах контроллер
+var uiController = (function () {})();
+
+// Санхүүтэй ажиллах контроллер
+var financeController = (function () {})();
+
+// Програттын холбогч контроллер
+var appController = (function (uiController, financeController) {})(
+  uiController,
+  financeController
+);
