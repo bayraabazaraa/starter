89de4c13b47d60ed280b91e4abc2e845358c73a6
bayraaqi <bayraabazaraa@gmail.com>
2020-05-26 19:14:52 +0800

    Event listenetuudiig uusgej ugluu

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ var uiController = (function () {})();
 var financeController = (function () {})();
 
 // Програттын холбогч контроллер
-var appController = (function (uiController, financeController) {})(
-  uiController,
-  financeController
-);
+var appController = (function (uiController, financeController) {
+  var ctrlAddItem = function () {
+    // 1. Оруулах өгөгдлийг дэлгэцээс олж авах.
+    console.log("Дэлгэцээс өгөгдлөө авах хэсэг");
+    // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
+    // 4. Төсвийг тооцоолно.
+    // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+  };
+
+  document.querySelector(" .add__btn").addEventListener("click", function () {
+    ctrlAddItem();
+  });
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+})(uiController, financeController);
